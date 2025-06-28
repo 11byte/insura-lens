fadccf5933ed1e1c09831b562fb7cc8a171f1433
Omkar Chandgaonkar <omkarchandgaonkar.com>
2025-06-28 16:47:50 +0530

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: {
+    chrome: "readonly",
+  },
+  extends: ["react-app", "eslint:recommended"],
+  rules: {
+    // You can add more rules here if needed
+  },
+};
